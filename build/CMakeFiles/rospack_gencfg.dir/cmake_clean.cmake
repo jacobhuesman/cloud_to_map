7f4b33e4ae857df454579120519483946a5c9b1b
Jacob Huesman <jakehuesman@gmail.com>
2015-10-09 16:25:21 -0500

    Created .gitignore file

--- build/CMakeFiles/rospack_gencfg.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/rospack_gencfg"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/rospack_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- build/CMakeFiles/rospack_gencfg_real.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/rospack_gencfg_real"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/rospack_gencfg_real.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
