d363f741e454462d79d5e1b196c62c7e8e142ea7
Jacob Huesman <jakehuesman@gmail.com>
2015-10-09 17:55:32 -0500

    Cleaned up cloud_to_map package and migrated to catkin workspace

--- devel/setup.sh
+++ devel/setup.sh
@@ -8,7 +8,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/jake/ros_ws/cloud_to_map/devel}
+: ${_CATKIN_SETUP_DIR:=/home/jake/cloud_to_map/devel}
 _SETUP_UTIL="$_CATKIN_SETUP_DIR/_setup_util.py"
 unset _CATKIN_SETUP_DIR
 
